6a0b99fe746473e38b88549b867fdc51336405aa
Gabriel 5illva <138382282+gabriel5illva@users.noreply.github.com>
2024-11-26 14:59:47 -0300

    Create server.js

---
+++ calculadorateste/server.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.post("/calculadorateste", (req, res) => {
+    const { correntinhas, baixissimo, baixo, meioAlto, alto, duploAlto, altissimo } = req.body;
+
+    const tempos = {
+        correntinhas: 2,
+        baixissimo: 2.5,
+        baixo: 3,
+        meioAlto: 3.5,
+        alto: 4,
+        duploAlto: 4.5,
+        altissimo: 5,
+    };
+
+    const tempoTotal = 
+        (correntinhas * tempos.correntinhas) +
+        (baixissimo * tempos.baixissimo) +
+        (baixo * tempos.baixo) +
+        (meioAlto * tempos.meioAlto) +
+        (alto * tempos.alto) +
+        (duploAlto * tempos.duploAlto) +
+        (altissimo * tempos.altissimo);
+
+    const minutos = Math.floor(tempoTotal / 60);
+    const segundos = Math.round(tempoTotal % 60);
+
+    res.json({ minutos, segundos });
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
